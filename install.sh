b8a5f871a8107ad1713275a4f973b51fdb861f7f
root <root@localhost.localdomain>
2020-01-16 11:23:11 -0500

    commit

--- install.sh
+++ install.sh
@@ -2,9 +2,29 @@
 
 export PWD_START=$(pwd)
 
-########################## Dependances ##########################
-apt-get install dialog
-apt-get install xmlstarlet
+checkPackageIsInstalled() {
+	if [[ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]]
+		then echo "no"
+		else echo "yes"
+	fi
+}
+
+installRequirements() {
+	for pack in `cat ./$1`
+	do
+		if [[ $(checkPackageIsInstalled $pack) = "no" ]]
+		then
+			echo "Y" | apt-get install $pack
+		else
+			echo "$pack is installed well."
+		fi
+	done
+}
+
+########################## First Dependances ##########################
+echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mFirst Dependences\e[92m                           \n--------------------------------------------------------------\e[0m"
+installRequirements first_requirements.txt
+
 
 nodes=$(xmlstarlet sel -T -t -m "//root" -m '*' -v "name()" -n tools.xml)
 for node in $nodes; do
@@ -13,7 +33,7 @@ for node in $nodes; do
 	eval "tab_$node=()"
 	int=1
 	for tool in $tools; do
-		eval "tab_$node+=($int $tool on)"
+		eval "tab_$node+=($int $tool off)"
 		let "int+=1"
 	done
 done
@@ -31,27 +51,9 @@ done
 clear
 mv /opt/Teeth /opt/osint/ 2>/dev/null
 
-########################## Dependances ##########################
-echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mDependences\e[92m                           \n--------------------------------------------------------------\e[0m"
-echo "Y" | apt-get install npm
-echo "Y" | apt-get install git
-echo "Y" | apt-get install golang
-apt-get install rpcbind
-apt-get install libnss3-tools;
-
-########################## Bash configuration ##########################
-echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mBash configuration\e[92m                           \n--------------------------------------------------------------\e[0m"
-cd ~;
-git init;
-git remote add origin https://github.com/TheThingGoesSkra/Bash_config.git;
-rm .bashrc;
-git pull origin master;
-cd $PWD_START;
-
-########################## System optimisation ##########################
-echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mSystem optimisation\e[92m                           \n--------------------------------------------------------------\e[0m"
-echo "vm.swappiness=10" >> /etc/sysctl.conf
-sed -i -e 's/\(GRUB_TIMEOUT=\).*/\10/' /etc/default/grub
+########################## Futur Dependances ##########################
+echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mFutur Dependences\e[92m                           \n--------------------------------------------------------------\e[0m"
+installRequirements futur_requirements.txt
 
 for node in $nodes; do
 	var=choices_$node[@]
@@ -63,34 +65,9 @@ for node in $nodes; do
 	done
 done
 
-########################## Burp configuration ##########################
-echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mBurp configuration\e[92m                           \n--------------------------------------------------------------\e[0m"
-firefox --headless &
-export APP_PID=$!;
-sleep 5;
-sudo kill -SIGTERM $APP_PID;
-echo "y" | java -Djava.awt.headless=true -Xmx1g -jar /bin/burpsuite &
-export APP_PID=$!;
-sleep 30;
-wget -e use_proxy=yes -e http_proxy=http://127.0.0.1:8080 http://burp/cert -O cacert.der;
-sleep 5;
-sudo kill -SIGTERM $APP_PID;
-firefox --no-remote -CreateProfile proxy;
-folder=$(find ~/.mozilla/firefox/ -maxdepth 1 -type d -name '*.proxy' -print -quit);
-certutil -A -n Burp -t "CT,c,c" -d "${folder}" -i cacert.der;
-cat prefs.js.old > "$folder/prefs.js";
-sudo openssl x509 -in cacert.der -inform DER -out burp.crt;
-sudo cp burp.crt /usr/local/share/ca-certificates/burp.crt;
-update-ca-certificates;
-
-
 ########################## Firefox extensions ##########################
 echo -e "\e[92m--------------------------------------------------------------\n                         \e[0mFirefox extensions\e[92m                           \n--------------------------------------------------------------\e[0m"
-wget https://addons.mozilla.org/firefox/downloads/file/3398269/max_hackbar-4.7-fx.xpi;
-wget https://addons.mozilla.org/firefox/downloads/file/3360387/http_request_maker-1.3-fx.xpi;
-wget  https://addons.mozilla.org/firefox/downloads/file/3477702/wappalyzer-5.9.0-fx.xpi;
-firefox max_hackbar-4.7-fx.xpi http_request_maker-1.3-fx.xpi wappalyzer-5.9.0-fx.xpi
-export APP_PID=$!;
+firefox *.xpi
 pfolder=$(find ~/.mozilla/firefox/ -maxdepth 1 -type d -name '*.proxy' -print -quit);
 dfolder=$(find ~/.mozilla/firefox/ -maxdepth 1 -type d -name '*.default' -print -quit);
 cp -r $dfolder/extensions $pfolder/extensions
